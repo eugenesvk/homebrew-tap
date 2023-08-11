2892b023b34a68eb24e9da02a968f8d83968077d
eugenesvk <eugenesvk@users.noreply.github.com>
2023-08-12 01:16:07 +0700

    _test downgrade

--- Formula/dupe-krill.rb
+++ Formula/dupe-krill.rb
@@ -1,7 +1,7 @@
 class DupeKrill < Formula
   desc "Fast file deduplicator"
   homepage "https://github.com/kornelski/dupe-krill"
-  url "https://github.com/kornelski/dupe-krill/releases/download/v1.4.8/dupe-krill-1.4.8-macOS.tar.xz"
+  url "https://github.com/kornelski/dupe-krill/releases/download/v1.4.7/dupe-krill-1.4.7-macOS.tar.xz"
   sha256 "d94eb8b0b87fac83e286182ecab8bef50fe97c2d3941b5c4a04340a03913f273"
   license "MIT"
 
