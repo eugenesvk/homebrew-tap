46bad7d54bd99533a022ad05baebb36a37b8dd6d
eugenesvk <eugenesvk@users.noreply.github.com>
2025-07-21 00:29:41 +0700

    🐞fix Linux audit fail on missing sha by adding a fake one

--- Casks/oculante.rb
+++ Casks/oculante.rb
@@ -3,7 +3,9 @@ cask "oculante" do
 
   version "0.9.2"
   sha256 arm:   "c3a59da9254c4b0912662c8fa6f0670551500481ee904270602149a33fdbd292",
-         intel: "c17742e6ebb28eb27f4696e43f5cb1446ac5636be83f051a2ddd60817500f698"
+         intel: "c17742e6ebb28eb27f4696e43f5cb1446ac5636be83f051a2ddd60817500f698",
+         arm64_linux:  "0",
+         x86_64_linux: "0"
 
   url "https://github.com/woelper/oculante/releases/download/#{version}/oculante_mac_#{arch}.zip"
   name "oculante"
