9112da1057885ece5b1fe27c9e35383c3a7fcc8b
eugenesvk <eugenesvk@users.noreply.github.com>
2025-07-21 00:33:00 +0700

    fix style

--- Casks/oculante.rb
+++ Casks/oculante.rb
@@ -2,8 +2,8 @@ cask "oculante" do
   arch arm: "silicon", intel: "universal"
 
   version "0.9.2"
-  sha256 arm:   "c3a59da9254c4b0912662c8fa6f0670551500481ee904270602149a33fdbd292",
-         intel: "c17742e6ebb28eb27f4696e43f5cb1446ac5636be83f051a2ddd60817500f698",
+  sha256 arm:          "c3a59da9254c4b0912662c8fa6f0670551500481ee904270602149a33fdbd292",
+         intel:        "c17742e6ebb28eb27f4696e43f5cb1446ac5636be83f051a2ddd60817500f698",
          arm64_linux:  "0",
          x86_64_linux: "0"
 
